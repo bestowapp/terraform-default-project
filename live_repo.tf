322790ff7bd5862eab17adf7ed3e479eba958aae
Ryan Hanks <ryandhanks@gmail.com>
2021-11-28 13:05:57 -0600

    wip

--- live_repo.tf
+++ live_repo.tf
@@ -1,11 +1,11 @@
-locals {
-  live_repo_name = "live-${local.project_name}"
-}
-resource "github_repository" "live_environment_group" {
-  name = local.live_repo_name
-  visibility = "private"
-  template {
-    owner      = "bestowapp"
-    repository = "template-live-default-project-modules"
-  }
-}
+#locals {
+#  live_repo_name = "live-${local.project_name}"
+#}
+#resource "github_repository" "live_environment_group" {
+#  name = local.live_repo_name
+#  visibility = "private"
+#  template {
+#    owner      = "bestowapp"
+#    repository = "template-live-default-project-modules"
+#  }
+#}
--- live_repo_secrets.tf
+++ live_repo_secrets.tf
@@ -1,21 +1,21 @@
-
-locals {
-  secrets = tomap({
-    "billing_account" : var.billing_account,
-    "name" : var.name,
-    "organization_id" : var.organization_id,
-    "project_id" : module.default_project.project_id,
-    "service_account_email" : module.default_project.service_account_email,
-    "service_account_id" : module.default_project.service_account_id,
-    "state_bucket_name" : google_storage_bucket.state.name,
-  })
-}
-
-resource "github_actions_secret" "bucket_name" {
-  repository      = github_repository.live_environment_group.name
-  for_each        = local.secrets
-  secret_name     = each.key
-  plaintext_value = each.value
-}
-
-
+#
+#locals {
+#  secrets = tomap({
+#    "billing_account" : var.billing_account,
+#    "name" : var.name,
+#    "organization_id" : var.organization_id,
+#    "project_id" : module.default_project.project_id,
+#    "service_account_email" : module.default_project.service_account_email,
+#    "service_account_id" : module.default_project.service_account_id,
+#    "state_bucket_name" : google_storage_bucket.state.name,
+#  })
+#}
+#
+#resource "github_actions_secret" "bucket_name" {
+#  repository      = github_repository.live_environment_group.name
+#  for_each        = local.secrets
+#  secret_name     = each.key
+#  plaintext_value = each.value
+#}
+#
+#
