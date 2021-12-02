9fbdd22e254425ba821b4cdf7d0b69edcada11c7
Ryan Hanks <ryandhanks@gmail.com>
2021-12-01 19:52:49 -0600

    Add service account admin role binding

--- role_bindings.tf
+++ role_bindings.tf
@@ -1,12 +1,13 @@
 locals {
   roles = toset([
-    "roles/storage.admin", "roles/compute.loadBalancerAdmin", "roles/dns.admin", "roles/iam.securityAdmin"
-#    "roles/firebase.admin", "roles/firebase.managementServiceAgent"
+    "roles/storage.admin", "roles/compute.loadBalancerAdmin", "roles/dns.admin", "roles/iam.securityAdmin",
+    "roles/iam.serviceAccountAdmin",
+    #    "roles/firebase.admin", "roles/firebase.managementServiceAgent"
   ])
 }
 resource "google_project_iam_member" "default_service_account_membership" {
   project  = module.default_project.project_id
   for_each = local.roles
   role     = each.key
-  member = "serviceAccount:${module.default_project.service_account_email}"
+  member   = "serviceAccount:${module.default_project.service_account_email}"
 }
